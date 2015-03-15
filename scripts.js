4772fc699b238782fbe6a46933f2dcb09b9bd163
Angelina-Panteley <lina.angel.95@mail.ru>
2015-03-15 13:16:22 +0300

    Сохранение сообщений

--- scripts.js
+++ scripts.js
@@ -1,72 +1,88 @@
-var index=-1;
-function run(){
+var UserName="";
+var messageList = [];
+function run1(){
+	restore();
+	document.getElementById("loginChanged").innerHTML="Welcome to SweetChat, "+UserName+"!";
+	document.addEventListener('click', delegateEvent);
+	var allMessages = restore() || [ ];
 
+	createAllMessages(allMessages);
 }
 function changeLogin()
  {
-      var text, text2;
-	  text=document.getElementById("login").value;
-	  if(text!=""){
-	      text2="Welcome to SweetChat, "+text+"!";
+      var text2;
+	  UserName=document.getElementById("login").value;
+	  if(UserName!=""){
+	      text2="Welcome to SweetChat, "+UserName+"!";
 	      document.getElementById("loginChanged").innerHTML=text2;
 	  }
+	  store(messageList);
  }
+ function delegateEvent(evtObj) {
+	if(evtObj.type === 'click'
+		&& evtObj.target.classList.contains('button'))
+		changeLogin();
+	if(evtObj.type === 'click'
+		&& evtObj.target.classList.contains('button6'))
+		onAddButtonClick();
+}
+function createAllMessages(allMessages) {
+	for(var i = 0; i < allMessages.length; i++)
+		addMail(allMessages[i]);
+}
+function onAddButtonClick(){
+	var mailText = document.getElementById('mailText');
+	var newMessage = theMessage(mailText.value,localStorage.getItem("UserName"));
 
-  function addRow() {
-          
-   var myName = document.getElementById("mail");
-   var table = document.getElementById("myTableData");
-   if(document.getElementById('login').value=="")
-{
-	 document.getElementById('login').value="User";
+	if(mailText.value == '')
+		return;
+
+	addMail(newMessage);
+	mailText.value = '';
+	store(messageList);
+} 
+function addMail(task) {
+	var item = createItem(task);
+	var items = document.getElementsByClassName('items')[0];
+
+	messageList.push(task);
+	items.appendChild(item);
 }
- if(index<0)
-    {      
-   	 var rowCount = table.rows.length;
-   	 var row = table.insertRow(rowCount);
-	 row.insertCell(0).innerHTML= '<input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input>';//<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
-  	 row.insertCell(1).innerHTML= '<input class="button5" onClick="Javacsript:editRow(this)" type="submit" title="Редактировать сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input>';//<input type="button" value = "Edit" onClick="Javacsript:editRow(this)">';
-	 row.insertCell(2).innerHTML = document.getElementById('login').value+":";
- 	 row.insertCell(3).innerHTML = myName.value;  
+
+function createItem(task){
+    var message = document.createElement('div');
+	
+	message.innerHTML = '<table><tr><td style="width:25%"><b>' + task.nickname + ':</b></td>                      <td width=70% style="word-wrap: break-word" onBlur="makeUneditable()">' + task.description + '</td>                      <td style="padding-left: 5px">                                            <img src="http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A" onClick="editMessage()" style="cursor: pointer"></img>                      <img src="http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI" onClick="deleteMessage()" style="cursor: pointer"</img>                      </td></tr></table>';
+	
+	return message.firstChild;
 }
-    if(index>-1)
-    {
-	table.deleteRow(index);   
-	var row = table.insertRow(index);
-	row.insertCell(0).innerHTML= '<input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input>';//<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
-	row.insertCell(1).innerHTML= '<input class="button5" onClick="Javacsript:editRow(this)" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input>';//<input type="button" value = "Edit" onClick="Javacsript:editRow(this)">';  
-	row.insertCell(2).innerHTML = document.getElementById('login').value+":";
-	row.insertCell(3).innerHTML = myName.value;
-index=-1;
-    }
- 
- //var myMail = document.getElementById("mail");
- 
- //var tableContent="<tr>";
- //tableContent+="<td width=60% valign='bottom'>"+'<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>'+"</td>";
- //if(document.getElementById('login').value=="")
- //{
-//	 document.getElementById('login').value="User";
- //}
-// tableContent+="<td width=40% >"+"<font color='black' align='right' size=4px style='vertical-align:'top';'>"+"<b>"+document.getElementById('login').value+":"+"</b>"+"</font>"+"<br>"+myMail.value+"</td>";
-// myTableData.innerHTML+=tableContent;
- document.getElementById('mail').value='';
+
+function store(listToSave) {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+	localStorage.setItem("UserName",UserName );
+	localStorage.setItem("messageList", JSON.stringify(listToSave));
 }
- 
-function deleteRow(obj) {
-      
-    var index = obj.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("myTableData");
-    table.deleteRow(index);
-    
+function restore() {
+	var item = localStorage.getItem("messageList");
+	UserName = localStorage.getItem("UserName");
+	return item && JSON.parse(item);
 }
- function editRow(obj) {
-    index = obj.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("myTableData");
-    document.getElementById("mail").value=table.rows[index].cells[3].innerHTML;
-}
-function load() {
-    //changeLogin();
-    console.log("Page load finished");
-    
-}
+
+var uniqueId = function() {
+	var date = Date.now();
+	var random = Math.random() * Math.random();
+
+	return Math.floor(date * random).toString();
+};
+
+var theMessage = function(text, name) {
+	return {
+		description:text,
+		nickname:name,
+		id: uniqueId()
+	};
+};
+
